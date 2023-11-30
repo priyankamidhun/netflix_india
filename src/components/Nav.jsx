ea1071e724de10e33bb26dad007234b428252f84
priyankamidhun <priyankamidhun123@gmail.com>
2023-11-30 10:42:26 +0530

    netflix project

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import './Nav.css'
+function Nav() {
+    return (
+        <>
+            <div className="nav">
+                <img className="nav_image" src="https://davidblaine.com/wp-content/uploads/2017/04/netflix-logo.png" alt="" width={'160px'} />
+            </div>
+        </>
+    )
+}
+export default Nav;
